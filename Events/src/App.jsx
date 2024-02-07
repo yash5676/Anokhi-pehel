14454c65d169b50cd6ba6ce00e3c6cedbc7fd3d1
yash5676 <yashkumargoyal1804@gmail.com>
2024-02-07 08:30:41 +0530

    add css

--- Events/src/App.jsx
+++ Events/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Homepage from "./pages/Homepage";
 import './App.css'
 
